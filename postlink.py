8de89245e7e551952e04ca73c00182c9d319d4e5
adrian ford <aiddy@aiddy.com>
2015-10-15 16:55:35 +0100

    Fixed bug printing error codes (int to string). Added env for running on Unix/Mac OS X

--- postlink.py
+++ postlink.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # Python script to post a link to delicious
 import getpass
 import sys
@@ -36,7 +37,7 @@ r = requests.put(call, auth=(user, passwd), params=payload)
 if r.status_code == 200:
 	print "sumbitted okay"
 else:
-	print "error submitting. status code: " + r.status_code 
+	print "error submitting. status code: " + str(r.status_code) 
 		
 
 
