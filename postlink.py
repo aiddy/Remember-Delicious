23b7bf4f6aadcd1c8bbf361acde8bdf9a200fa1b
Aiddy <github@aiddy.com>
2015-10-16 09:48:49 +0100

    Added custom user agent per Delicious API guidanace

--- postlink.py
+++ postlink.py
@@ -17,10 +17,8 @@ if len(sys.argv) != 4:
 	print "   Tags\t\tA string providing a comma delimited list of tags for the link"
 	exit (-1)
 
-linkurl = sys.argv[1]  
-linkdescription = sys.argv[2]  
-linktags = sys.argv[3]  
-payload = {"url": 	linkurl, "description": linkdescription, "tags": linktags}
+headers = {'user-agent': 'Remember-Delicious/0.0.1'}
+payload = {"url": 	sys.argv[1], "description": sys.argv[2], "tags": sys.argv[3]}
 
 print "Posting link to delicious for " + user
 print "\t\t" + payload['url']
@@ -32,7 +30,10 @@ passwd = getpass.getpass()
 
 call = "https://api.del.icio.us/v1/posts/add"
 
-r = requests.put(call, auth=(user, passwd), params=payload)
+r = requests.put(call, 
+	auth=(user, passwd), 
+	params=payload
+	headers=headers)
 if r.status_code == 200:
 	print "sumbitted okay"
 else:
